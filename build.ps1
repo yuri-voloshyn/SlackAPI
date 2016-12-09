d0404c5a0cd8a9ecf5ecc80be41ab70fcf797baf
Gregoire Pailler <gregoire@pailler.fr>
2016-12-09 12:03:06 +0800

    Redirect error output to standard output

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ function Exec
     [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
     [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
   )
-  & $cmd
+  & $cmd 2>&1
   if ($lastexitcode -ne 0) {
     throw ("Exec: " + $errorMessage)
   }
